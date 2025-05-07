6238d685c7774546d5d420b93656dcb255b09733
takamori-saigo <anamneshev1999@gmail.com>
2025-05-07 23:56:43 +0500

    add startproject

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		warnOnUnsupportedTypeScriptVersion: false,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'prettier',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'plugin:jsx-a11y/recommended',
+		'plugin:eslint-comments/recommended',
+	],
+	rules: {
+		semi: [2, 'always'],
+		quotes: [2, 'single', { avoidEscape: true }],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'@typescript-eslint/no-var-requires': 'off',
+		'react/prop-types': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
+};
